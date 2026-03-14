fcabb16c6a56984baebb9fd9dce1b4991d36367c
lhengyu35-oss <lhengyu35@gmail.com>
2026-03-14 15:30:59 +0800

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+// 1. 在这里引入 Analytics 组件
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* 2. 在这里添加 Analytics 标签 */}
+    <Analytics />
   </StrictMode>,
 );
